c12a00529ccfef16a93d825eff17b73b79dad113
pc710 <pc710@iucesmag.edu.co>
2018-04-05 13:15:36 -0500

    1

--- app/src/main/java/com/example/aula7/colores/MainActivity.java
+++ app/src/main/java/com/example/aula7/colores/MainActivity.java
@@ -2,12 +2,67 @@ package com.example.aula7.colores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewName;
+    Button buttonOpen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewName=(TextView) findViewById(R.id.id_tv);
+        buttonOpen = (Button) findViewById(R.id.id_btn_ira);
+
+        textViewName.setText("Yojan Sanchez");
+
+        buttonOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Hola mundo", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.id_menu1:
+                Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.id_menu2:
+                Toast.makeText(this, "Item2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
     }
 }
